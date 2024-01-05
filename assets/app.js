f1ecfd1ab7961bf155fdbb9c477a365141469f6d
Inessa <143314129+icollier77@users.noreply.github.com>
2024-01-05 19:53:03 +0000

    added the html elements

--- assets/app.js
+++ assets/app.js
@@ -4,10 +4,20 @@ console.log(today);
 $("#currentDay").text(today);
 
 
-// TODO: Present timeblocks for standard business hours when the user scrolls down.
- 
 // TODO: Color-code each timeblock based on past, present, and future when the timeblock is viewed.
- 
+// get the current time
+const currentTime = dayjs().format('H');
+console.log(currentTime);
+
+// compare currentTime with all 
+const hourBlocks = $('.hour');
+console.log(hourBlocks);
+
+hourBlocks.each(hour => {
+    console.log(hour.attr('id'));
+})
+
+
 // TODO: Allow a user to enter an event when they click a timeblock
 
 // TODO: Save the event in local storage when the save button is clicked in that timeblock.
