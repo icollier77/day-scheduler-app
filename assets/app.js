6e6ef760b25cda4ce9db60b8958ae077e794d76e
Inessa <143314129+icollier77@users.noreply.github.com>
2024-01-05 22:37:46 +0000

    Added event listener on hourly input

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,5 @@
 // Display the current day at the top of the calender when a user opens the planner.
 const today = dayjs().format('dddd, D MMM YYYY');
-console.log(today);
 $("#currentDay").text(today);
 
 // get the current time
@@ -12,18 +11,26 @@ const currentTime = parseInt(dayjs().format('H'));
 const hourBlocks = $('.hour');
 hourBlocks.each(function() {
     let blockHour = parseInt($(this).attr('id'));
-    console.log(blockHour, typeof blockHour);
     if (blockHour < currentTime) {
         $(this).next().addClass('past');
     } else if (blockHour === currentTime) {
         $(this).next().addClass('present');
     } else if (blockHour > currentTime) {
         $(this).next().addClass('future');
-    }
-})
+    };
+});
 
 
 // TODO: Allow a user to enter an event when they click a timeblock
+// create 
+// add click event (through delegation) on buttons
+// function addTask() {
+    $('.container').on('click', 'button', function(event) {
+    let hourInput = $(this).prev().val();
+    
+    });
+// };
+
 
 // TODO: Save the event in local storage when the save button is clicked in that timeblock.
 
